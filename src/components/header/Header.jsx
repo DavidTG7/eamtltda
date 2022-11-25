a86077a962865234e2bef2a93a3d0dd5466ae35a
DavidTG7 <davidthekoa@gmail.com>
2022-11-24 21:27:28 -0500

    Crear NavBar

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,27 +6,30 @@ import NavDropdown from "react-bootstrap/NavDropdown";
 export const Header = () => {
   return (
     <header>
-      <Navbar bg="light" expand="lg">
+      <Navbar bg="primary" variant="dark" expand="lg">
         <Container>
-          <Navbar.Brand href="#home">EAMLTADA</Navbar.Brand>
+          <Navbar.Brand href="#home" style={{ fontWeight: "bold" }}>
+            EAMLTADA
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
+              <NavDropdown title="Empresa" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Misión</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Visión</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Políticas de Calidad
                 </NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.3">
-                  Something
+                  Objetivos de Calidad
                 </NavDropdown.Item>
                 <NavDropdown.Divider />
                 <NavDropdown.Item href="#action/3.4">
                   Separated link
                 </NavDropdown.Item>
               </NavDropdown>
+              <Nav.Link href="#home">¿Quiénes somos?</Nav.Link>
+              <Nav.Link href="#link">Servicios</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
