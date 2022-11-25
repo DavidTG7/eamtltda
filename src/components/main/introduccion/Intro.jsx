63dc38c9bf0f6c195492074b7057e8dcf38f32f1
Juan José Figueroa Navarro <78628936+Juanjofigue97@users.noreply.github.com>
2022-11-24 21:25:01 -0500

    Carusel de Imagenes

--- src/components/main/introduccion/Intro.jsx
+++ src/components/main/introduccion/Intro.jsx
@@ -1,9 +1,54 @@
 import "./intro.css";
+import Carousel from 'react-bootstrap/Carousel';
+import fondo1 from '../../../imagenes/Fondo_login2.png';
+import fondo2 from '../../../imagenes/principios2.jpg';
+import fondo3 from '../../../imagenes/valores.jpg';
+
 
 export const Intro = () => {
   return (
-    <div className="intro">
-      <h2 className="intro-titulo">Hola Intro</h2>
+    <div>
+      <Carousel variant="dark">
+        {/* //Inicio del Item     */}
+        <Carousel.Item className="intro-carrusel-elemento">
+          <img
+            className="d-block w-100 intro-carrusel-imagen"
+            src={fondo1}
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>Somos una empresa...</h3>
+            <p>Podemos ofrecerte</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        {/* //Fin de un item */}
+        <Carousel.Item>
+          <img
+            className="d-block w-100 intro-carrusel-imagen"
+            src= {fondo2}
+            alt="Second slide"
+          />
+
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100 intro-carrusel-imagen"
+            src={fondo3}
+            alt="Third slide"
+          />
+
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
     </div>
   );
 };
