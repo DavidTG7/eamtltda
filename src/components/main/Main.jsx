5ffeca4e66feb01170e25d1358e9283459de172e
DavidTG7 <davidthekoa@gmail.com>
2022-11-27 22:35:55 -0500

    Borrar Componente QuienesSomos

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,14 +1,12 @@
 import { Empresa } from "./empresa/Empresa";
 import { Intro } from "./introduccion/Intro";
 import { Nosotros } from "./nosotros/Nosotros";
-import { QuienesSomos } from "./quienesSomos/QuienesSomos";
 import "./main.css";
 
 export const Main = () => {
   return (
     <main id="main-box">
       <Intro />
-      <QuienesSomos />
       <Empresa />
       <Nosotros />
     </main>
