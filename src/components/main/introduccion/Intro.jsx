9a6677810d9bf1e2abd2b001fb58804b232c1868
Juan José Figueroa Navarro <78628936+Juanjofigue97@users.noreply.github.com>
2022-11-27 22:25:08 -0500

    Introduccion

--- src/components/main/introduccion/Intro.jsx
+++ src/components/main/introduccion/Intro.jsx
@@ -8,10 +8,10 @@ import logo from '../../../imagenes/Logo_Editado.png';
 
 export const Intro = () => {
   return (
-    <div>
-      <Carousel variant="dark">
+    <div className="intro-carousel">
+      <Carousel slide={false} variant="dark">
         {/* //Inicio del Item     */}
-        <Carousel.Item className="intro-carrusel-elemento">
+        {/* <Carousel.Item className="intro-carrusel-elemento">
           <img
             className="d-block w-100 intro-carrusel-imagen"
             src={fondo1}
@@ -25,34 +25,8 @@ export const Intro = () => {
             />
             <p>EAMT LTDA. es una empresa colombiana con 25 años de experiencia en Consultoría y Asesoría que apoya las organizaciones en la auditoria de procesos, detección de riesgos, diseño de estrategias, ejecución de planes de acción y apoya las iniciativas de documentación, implementación y automatización de Procesos de Negocios. </p>
           </Carousel.Caption>
-        </Carousel.Item>
+        </Carousel.Item> */}
         {/* //Fin de un item */}
-        <Carousel.Item>
-          <img
-            className="d-block w-100 intro-carrusel-imagen"
-            src= {fondo2}
-            alt="Second slide"
-          />
-
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100 intro-carrusel-imagen"
-            src={fondo3}
-            alt="Third slide"
-          />
-
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
         <Carousel.Item>
           <div className="intro-contenedor">
             <div className="intro-contenedor-informacion">
@@ -67,6 +41,25 @@ export const Intro = () => {
               <img
               className="d-block w-100 intro-carrusel-imagen"
               src={fondo3}
+              alt="Third slide"
+              />  
+            </div>
+          </div>
+        </Carousel.Item>
+        <Carousel.Item>
+          <div className="intro-contenedor">
+            <div className="intro-contenedor-informacion">
+              <img
+                className="intro-carrusel-contenido-imagen"
+                src={logo}
+                alt="Logo"
+              />
+              <p>EAMT LTDA. es una empresa colombiana con 25 años de experiencia en Consultoría y Asesoría que apoya las organizaciones en la auditoria de procesos, detección de riesgos, diseño de estrategias, ejecución de planes de acción y apoya las iniciativas de documentación, implementación y automatización de Procesos de Negocios. </p>
+            </div>
+            <div className="intro-contenedor-imagen">
+              <img
+              className="d-block w-100 intro-carrusel-imagen"
+              src={fondo2}
               alt="Third slide"
               />  
             </div>
--- src/components/main/nosotros/Nosotros.jsx
+++ src/components/main/nosotros/Nosotros.jsx
@@ -1,14 +1,39 @@
 import "./nosotros.css";
 import { FaHeart } from "react-icons/fa";
+import logo from '../../../imagenes/Logo_Editado.png';
 
 export const Nosotros = () => {
   return (
-    <div>
-      <div className="nosotros-container">
-        <div className="nosotros-container-item">
-            <FaHeart className="nosotros-container-img"/>
+    <div className="nosotros-container-general">
+      {/* <img className="nosotros-container-general-img" src={logo} alt="logo"></img> */}
+      <div className="nosotros-container-compañia">
+        <h2>Compañia</h2>
+        <div className="nosotros-container">
+          <div className="nosotros-container-item">
+              <FaHeart className="nosotros-container-img"/>
+              <p>Nosotros</p>
+          </div>
+          <div className="nosotros-container-item">
+              <FaHeart className="nosotros-container-img"/>
+              <p>Trayectoria</p>
+          </div>
+          <div className="nosotros-container-item">
+              <FaHeart className="nosotros-container-img"/>
+              <p>Clientes</p>
+          </div>
+          <div className="nosotros-container-item">
+              <FaHeart className="nosotros-container-img"/>
+              <p>Donde estamos</p>
+          </div>
+          <div className="nosotros-container-item">
+              <FaHeart className="nosotros-container-img"/>
+              <p>Donde estamos</p>
+          </div>
+          <div className="nosotros-container-item">
+              <FaHeart className="nosotros-container-img"/>
+              <p>Donde estamos</p>
+          </div>
         </div>
-        
       </div>
     </div>
   );
