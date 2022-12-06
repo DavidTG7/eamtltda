396b1cf5dff5d9cfe1c951f04a6994d26a55fd6f
Juan José Figueroa Navarro <78628936+Juanjofigue97@users.noreply.github.com>
2022-12-06 00:27:57 -0500

    Servicios Actualizados

--- src/components/main/servicios/Servicios.jsx
+++ src/components/main/servicios/Servicios.jsx
@@ -15,6 +15,8 @@ import IInterventoria from './image/interventoria.png';
 import ISistemas from './image/Sistemas.png';
 import Button from 'react-bootstrap/Button';
 
+import { LineDivisor } from "./servicios.styled";
+
 export const Servicios = () => {
   return (
     <div>
@@ -34,15 +36,11 @@ export const Servicios = () => {
             <li>Administración de incidentes de Seguridad</li>
             <li>Diseño de procedimientos de seguridad</li>
           </ul>
-          <Button variant="success">Conoce más</Button>
+          <LineDivisor color="rgba(29, 250, 0, 1)" />
+          {/* <Button variant="success">Conoce más</Button> */}
         </div>
       </div>
-      <div className="servicios-contenedor" id="AnalisSeguridad">
-        <img
-          className="servicios-contenedor-img"
-          src={IAnalisisSeguridad}
-          alt="Logo"
-        />
+      <div className="servicios-contenedor invertido" id="AnalisSeguridad">
         <div className="servicios-contenedor-texto" >
           <h3>Análisis de Seguridad</h3>
           <ul className="servicios-contenedor-texto-caracteristicas">
@@ -50,8 +48,15 @@ export const Servicios = () => {
             <li>Análisis de madurez de seguridad</li>
             <li>Diseño de Soluciones de seguridad</li>
           </ul>
-          <Button variant="success">Conoce más</Button>
+          <LineDivisor color="rgba(222, 25, 0, 0.8)" />
+          <LineDivisor color="rgba(222, 25, 0, 0.8)" />
+          {/* <Button variant="success">Conoce más</Button> */}
         </div>
+        <img
+          className="servicios-contenedor-img"
+          src={IAnalisisSeguridad}
+          alt="Logo"
+        />
       </div>
       <div className="servicios-contenedor" id="Control">
         <img
@@ -68,15 +73,11 @@ export const Servicios = () => {
             <li>Políticas de control de acceso</li>
             <li>Estándares de control de acceso</li>
           </ul>
-          <Button variant="success">Conoce más</Button>
+          <LineDivisor color="rgba(189, 25, 230, 0.8)" />
+          {/* <Button variant="success">Conoce más</Button> */}
         </div>
       </div>
-      <div className="servicios-contenedor">
-        <img
-          className="servicios-contenedor-img"
-          src={IPoliticas}
-          alt="Logo"
-        />
+      <div className="servicios-contenedor invertido">
         <div className="servicios-contenedor-texto" id="PSeguridad">
           <h3>Políticas de Seguridad</h3>
           <ul className="servicios-contenedor-texto-caracteristicas">
@@ -84,8 +85,14 @@ export const Servicios = () => {
             <li>Capacitación en seguridad</li>
             <li>Aplicación de normatividad estatal</li>
           </ul>
-          <Button variant="success">Conoce más</Button>
+          <LineDivisor color="rgba(20, 234, 209, 0.8)" />
+          {/* <Button variant="success">Conoce más</Button> */}
         </div>
+        <img
+          className="servicios-contenedor-img"
+          src={IPoliticas}
+          alt="Logo"
+        />
       </div>
       <div className="servicios-contenedor" id="ISeguridad">
         <img
@@ -101,23 +108,26 @@ export const Servicios = () => {
             <li>Monitoreo de incidentes de seguridad</li>
             <li>Administración de incidentes de seguridad</li>
           </ul>
-          <Button variant="success">Conoce más</Button>
+          <LineDivisor color="rgba(226, 16, 18, 0.8)" />
+          <LineDivisor color="rgba(226, 16, 18, 0.8)" />
+          {/* <Button variant="success">Conoce más</Button> */}
         </div>
       </div>
-      <div className="servicios-contenedor" id="Proteccion">
-        <img
-          className="servicios-contenedor-img"
-          src={IProteccion}
-          alt="Logo"
-        />
+      <div className="servicios-contenedor invertido " id="Proteccion">
         <div className="servicios-contenedor-texto"  >
           <h3>Protección</h3>
           <ul className="servicios-contenedor-texto-caracteristicas">
             <li>Diseño e implementación de controles de seguridad</li>
             <li>Monitoreo de controles de seguridad.</li>
           </ul>
-          <Button variant="success">Conoce más</Button>
+          <LineDivisor color="rgba(229, 153, 39, 0.8)" />
+          {/* <Button variant="success">Conoce más</Button> */}
         </div>
+        <img
+          className="servicios-contenedor-img"
+          src={IProteccion}
+          alt="Logo"
+        />
       </div>
       <div className="servicios-contenedor" id="PerSeguridad">
         <img
@@ -130,15 +140,12 @@ export const Servicios = () => {
           <ul className="servicios-contenedor-texto-caracteristicas">
             <li>Recurso Especializado en Seguridad Informática de acuerdo a las directrices de las normas ISO 17799 - 27001 - 27002 (SGSI)</li>
           </ul>
-          <Button variant="success">Conoce más</Button>
+          <LineDivisor color="rgba(57, 210, 19, 0.8)" />
+          <LineDivisor color="rgba(57, 210, 19, 0.8)" />
+          {/* <Button variant="success">Conoce más</Button> */}
         </div>
       </div>
-      <div className="servicios-contenedor" id="Auditoria">
-        <img
-          className="servicios-contenedor-img"
-          src={IAuditoria}
-          alt="Logo"
-        />
+      <div className="servicios-contenedor invertido" id="Auditoria">
         <div className="servicios-contenedor-texto"  >
           <h3>Auditoria</h3>
           <ul className="servicios-contenedor-texto-caracteristicas">
@@ -146,8 +153,14 @@ export const Servicios = () => {
             <li>Auditoria a los sistemas de información</li>
             <li>Auditoria en la aplicación de controles</li>
           </ul>
-          <Button variant="success">Conoce más</Button>
+          <LineDivisor color="rgba(50, 225, 216, 0.8)" />
+          {/* <Button variant="success">Conoce más</Button> */}
         </div>
+        <img
+          className="servicios-contenedor-img"
+          src={IAuditoria}
+          alt="Logo"
+        />
       </div>
       <div className="servicios-contenedor" id="Procesos">
         <img
@@ -155,7 +168,7 @@ export const Servicios = () => {
           src={IProcesos}
           alt="Logo"
         />
-        <div className="servicios-contenedor-texto" id="ISeguridad">
+        <div className="servicios-contenedor-texto">
           <h3>Procesos y Procedimientos</h3>
           <ul className="servicios-contenedor-texto-caracteristicas">
             <li>Diseño de procesos y procedimientos</li>
@@ -164,22 +177,24 @@ export const Servicios = () => {
             <li>Diseño e implementación del sistema de gestión de calidad (SGC)</li>
             <li>Diseño de politicas y objetivos de calidad</li>
           </ul>
-          <Button variant="success">Conoce más</Button>
+          <LineDivisor color="rgba(50, 225, 216, 0.8)" />
+          {/* <Button variant="success">Conoce más</Button> */}
         </div>
       </div>
-      <div className="servicios-contenedor" id="SGC">
-        <img
-          className="servicios-contenedor-img"
-          src={IMonitoreo}
-          alt="Logo"
-        />
+      <div className="servicios-contenedor invertido" id="SGC">
         <div className="servicios-contenedor-texto">
           <h3>SGC.</h3>
           <ul className="servicios-contenedor-texto-caracteristicas">
             <li>Diseño, implementación y puesta en marcha del sistema de gestión de calidad, de acuerdo a las directrices y políticas de la Norma ISO 9001 versióm 2000.</li>
           </ul>
-          <Button variant="success">Conoce más</Button>
+          <LineDivisor color="rgba(241, 222, 36, 0.8)" />
+          {/* <Button variant="success">Conoce más</Button> */}
         </div>
+        <img
+          className="servicios-contenedor-img"
+          src={IMonitoreo}
+          alt="Logo"
+        />
       </div>
       <div className="servicios-contenedor" id="AuditoriaSistemas">
         <img
@@ -192,23 +207,25 @@ export const Servicios = () => {
           <ul className="servicios-contenedor-texto-caracteristicas">
             <li>Recurso humano especializado en auditoria de sistemas y aplicacion de estándares internacionales a los sistemas de información de acuerdo a los modelos COBIT-COSO-MECI.</li>
           </ul>
-          <Button variant="success">Conoce más</Button>
+          <LineDivisor color="rgba(160, 233, 45, 0.8)" />
+          {/* <Button variant="success">Conoce más</Button> */}
         </div>
       </div>
-      <div className="servicios-contenedor" id="SoftwareWeb">
-        <img
-          className="servicios-contenedor-img"
-          src={IPortalWeb}
-          alt="Logo"
-        />
+      <div className="servicios-contenedor invertido" id="SoftwareWeb">
         <div className="servicios-contenedor-texto">
           <h3>Software y Portales Web</h3>
           <ul className="servicios-contenedor-texto-caracteristicas">
             <li>Desarrollo y puesta en marcha de aplicaciones especializadas y especificas para su empresa</li>
             <li>Creación de portales corporativos y sistemas de información</li>
           </ul>
-          <Button variant="success">Conoce más</Button>
+          <LineDivisor color="rgba(111, 90, 238, 0.8)" />
+          {/* <Button variant="success">Conoce más</Button> */}
         </div>
+        <img
+          className="servicios-contenedor-img"
+          src={IPortalWeb}
+          alt="Logo"
+        />
       </div>
       <div className="servicios-contenedor" id="Planeacion">
         <img
@@ -221,22 +238,24 @@ export const Servicios = () => {
           <ul className="servicios-contenedor-texto-caracteristicas">
             <li>Análisis, planeación y diseño de esquemas de contingencia, respaldo y recuperación a nivel de los datos dando continuidad al negocio</li>
           </ul>
-          <Button variant="success">Conoce más</Button>
+          <LineDivisor color="rgba(160, 233, 45, 0.8)" />
+          {/* <Button variant="success">Conoce más</Button> */}
         </div>
       </div>
-      <div className="servicios-contenedor" id="Redes">
-        <img
-          className="servicios-contenedor-img"
-          src={IRedes}
-          alt="Logo"
-        />
+      <div className="servicios-contenedor invertido" id="Redes">
         <div className="servicios-contenedor-texto">
           <h3>Redes</h3>
           <ul className="servicios-contenedor-texto-caracteristicas">
             <li>Diseño e implementación de redes alambricas e inalámbricas para la conexión y administración de recursos empresariales</li>
           </ul>
-          <Button variant="success">Conoce más</Button>
+          <LineDivisor color="rgba(189, 25, 230, 0.8)" />
+          {/* <Button variant="success">Conoce más</Button> */}
         </div>
+        <img
+          className="servicios-contenedor-img"
+          src={IRedes}
+          alt="Logo"
+        />
         </div>
         <div className="servicios-contenedor" id="Interventoria">
         <img
@@ -252,23 +271,25 @@ export const Servicios = () => {
             <li>Esquemas de comunicaciones y seguridad informática</li>
             <li>Aplicativos en producción</li>
           </ul>
-          <Button variant="success">Conoce más</Button>
+          <LineDivisor color="rgba(20, 234, 209, 0.8)" />
+          {/* <Button variant="success">Conoce más</Button>  */}
         </div>
         </div>
-        <div className="servicios-contenedor" id="AdmiSistemas">
-        <img
-          className="servicios-contenedor-img"
-          src={ISistemas}
-          alt="Logo"
-        />
+        <div className="servicios-contenedor invertido" id="AdmiSistemas">
         <div className="servicios-contenedor-texto">
           <h3>Administración de Sistemas</h3>
           <ul className="servicios-contenedor-texto-caracteristicas">
             <li>Configuración de servidores: DNS, PROXY, FIREWALL, CORREO, TERMINAL SERVICE, VPN y de aplicación de base de datos</li>
             <li>Admnistración, diseño y creación de planes de mantenimiento para vases de datos</li>
           </ul>
-          <Button variant="success">Conoce más</Button>
+          <LineDivisor color="rgba(189, 25, 230, 0.8)" />
+          {/* <Button variant="success">Conoce más</Button> */}
         </div>
+        <img
+          className="servicios-contenedor-img"
+          src={ISistemas}
+          alt="Logo"
+        />
       </div>
     </div>
   );
