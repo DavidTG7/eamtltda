7ea9b122caaf9d70440360650d167fc5289ffdb5
DavidTG7 <davidthekoa@gmail.com>
2022-12-06 00:48:30 -0500

    Actualizar componente Nosotros

--- src/components/main/nosotros/Nosotros.jsx
+++ src/components/main/nosotros/Nosotros.jsx
@@ -1,132 +1,229 @@
+// import "./nosotros.css";
+// import { } from "react-icons/fa";
+// import {  } from "react-icons/bi";
+// import {  } from "react-icons/cg";
+// import { MdSecurity,MdDangerous,MdAdminPanelSettings} from "react-icons/md";
+// import { RiTeamFill } from "react-icons/ri";
+// import { VscSourceControl,VscSettingsGear } from "react-icons/vsc";
+// import { ImBooks } from "react-icons/im";
+// import { AiOutlineFileProtect } from "react-icons/ai";
+// import { FcProcess } from "react-icons/fc";
+// import { SiWebmoney } from "react-icons/si";
+
+// import danger from './image/danger.svg';
+// import seguridad from './image/seguridad.png';
+// import procesos from './image/procesos.png';
+// import sistemas from './image/sistemas.png';
+// import plan from './image/plan.png';
+// import auditoria from './image/auditoria.png';
+// import auditoria_sistemas from './image/auditoria_sistemas.png';
+// import redes from './image/redes.png';
+// import interventoria from './image/interventoria.png';
+// import administracion from './image/administracion.png';
+
+// export const Nosotros = () => {
+//   return (
+//     <div className="nosotros-container-general" id="servicios">
+//       {/* <img className="nosotros-container-general-img" src={logo} alt="logo"></img> */}
+//       <div className="nosotros-container-compañia">
+//         <h2 className="gcSeIN">Servicios</h2>
+//         <div className="nosotros-container">
+//           <a href="#Riesgos" className="nosotros-container-item">
+//               <img
+//                 src={danger}
+//                 alt="Gestion de Riesgos"
+//                 className="nosotros-container-item-icons"/>
+//             <p>Gestion de Riesgos</p>
+//           </a>
+//           <a href="#AnalisSeguridad" className="nosotros-container-item">
+//               <MdSecurity className="nosotros-container-item-icons"/>
+//               <p>Analisis de Seguridad</p>
+//           </a>
+//           <a href="#Control" className="nosotros-container-item">
+//               <VscSourceControl className="nosotros-container-item-icons"/>
+//               <p>Control de Acesso</p>
+//           </a>
+//           <a href="#PSeguridad" className="nosotros-container-item">
+//               <ImBooks className="nosotros-container-item-icons"/>
+//               <p>Politicas de Seguridad</p>
+//           </a>
+//           <a href="#ISeguridad" className="nosotros-container-item">
+//               <MdDangerous className="nosotros-container-item-icons"/>
+//               <p>Incidentes de Seguridad</p>
+//           </a>
+//           <a href="#Proteccion" className="nosotros-container-item">
+//               <AiOutlineFileProtect className="nosotros-container-item-icons" />
+//               <p>Protección </p>
+//           </a>
+//           <a href="#PerSeguridad" className="nosotros-container-item">
+//           <img
+//                 src={seguridad}
+//                 alt="Gestion de Riesgos"
+//                 className="nosotros-container-item-icons"/>
+//               <p>Personal de Seguridad </p>
+//           </a>
+//           <a href="#Auditoria" className="nosotros-container-item">
+//           <img
+//                 src={auditoria}
+//                 alt="Gestion de Riesgos"
+//                 className="nosotros-container-item-icons"/>
+//               <p>Auditoria </p>
+//           </a>
+//           <a href="#Procesos" className="nosotros-container-item">
+//           <img
+//                 src={procesos}
+//                 alt="Gestion de Riesgos"
+//                 className="nosotros-container-item-icons"/>
+//               <p>Procesos y Procedimientos </p>
+//           </a>
+//           <a href="#SGC" className="nosotros-container-item">
+//           <img
+//                 src={sistemas}
+//                 alt="Gestion de Riesgos"
+//                 className="nosotros-container-item-icons"/>
+//               <p>SGC</p>
+//           </a>
+//           <a href="#AuditoriaSistemas" className="nosotros-container-item">
+//           <img
+//                 src={auditoria_sistemas}
+//                 alt="Gestion de Riesgos"
+//                 className="nosotros-container-item-icons"/>
+//               <p>Auditoria de Sistemas</p>
+//           </a>
+//           <a href="#SoftwareWeb" className="nosotros-container-item">
+//               <SiWebmoney className="nosotros-container-item-icons" />
+//               <p>Sotfware y Portal Web</p>
+//           </a>
+//           <a href="#Planeacion" className="nosotros-container-item">
+//           <img
+//                 src={plan}
+//                 alt="Gestion de Riesgos"
+//                 className="nosotros-container-item-icons"/>
+//               <p>Planeación</p>
+//           </a>
+//           <a href="#Redes" className="nosotros-container-item">
+//           <img
+//                 src={redes}
+//                 alt="Gestion de Riesgos"
+//                 className="nosotros-container-item-icons"/>
+//               <p>Redes</p>
+//           </a>
+//           <a href="#Interventoria" className="nosotros-container-item">
+//           <img
+//                 src={interventoria}
+//                 alt="Gestion de Riesgos"
+//                 className="nosotros-container-item-icons"/>
+//               <p>Interventoria</p>
+//           </a>
+//           <a href="#AdmiSistemas" className="nosotros-container-item">
+//           <img
+//                 src={administracion}
+//                 alt="Gestion de Riesgos"
+//                 className="nosotros-container-item-icons"/>
+//               <p>Administración de Sistemas</p>
+//           </a>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// // Nominacion de clases: BEM Block Element Modifier
+
+import gestionRiesgos from "./images/gestionRiesgos.svg";
+import analisisSeguridad from "./images/analisisSeguridad.svg";
+import controlAcceso from "./images/controlAcceso.svg";
+import politicasSeguridad from "./images/politicasSeguridad.svg";
+import inicidenteSeguridad from "./images/inicidenteSeguridad.svg";
+import proteccion from "./images/proteccion.svg";
+import personalSeguridad from "./images/personalSeguridad.svg";
+import auditoria from "./images/auditoria.svg";
+import procesosProcedimientos from "./images/procesosProcedimientos.svg";
+import sgc from "./images/sgc.svg";
+import auditoriaSistemas from "./images/auditoriaSistemas.svg";
+import software from "./images/software.svg";
+import planeacion from "./images/planeacion.svg";
+import redes from "./images/redes.svg";
+import interventoria from "./images/interventoria.svg";
 import "./nosotros.css";
-import { } from "react-icons/fa";
-import {  } from "react-icons/bi";
-import {  } from "react-icons/cg";
-import { MdSecurity,MdDangerous,MdAdminPanelSettings} from "react-icons/md";
-import { RiTeamFill } from "react-icons/ri";
-import { VscSourceControl,VscSettingsGear } from "react-icons/vsc";
-import { ImBooks } from "react-icons/im";
-import { AiOutlineFileProtect } from "react-icons/ai";
-import { FcProcess } from "react-icons/fc";
-import { SiWebmoney } from "react-icons/si";
 
+const servicios = [
+  {
+    nombre: "Gestión de Riesgos",
+    imagen: gestionRiesgos,
+  },
+  {
+    nombre: "Análisis de Seguridad",
+    imagen: analisisSeguridad,
+  },
+  {
+    nombre: "Conctrol de Acceso",
+    imagen: controlAcceso,
+  },
+  {
+    nombre: "Políticas de Seguridad",
+    imagen: politicasSeguridad,
+  },
+  {
+    nombre: "Incidentes de Seguridad",
+    imagen: inicidenteSeguridad,
+  },
+  {
+    nombre: "Protección",
+    imagen: proteccion,
+  },
+  {
+    nombre: "Personal de Seguridad",
+    imagen: personalSeguridad,
+  },
+  {
+    nombre: "Auditoria",
+    imagen: auditoria,
+  },
+  {
+    nombre: "Procesos y Procedimientos",
+    imagen: procesosProcedimientos,
+  },
+  {
+    nombre: "SGC",
+    imagen: sgc,
+  },
+  {
+    nombre: "Auditoria de Sistemas",
+    imagen: auditoriaSistemas,
+  },
+  {
+    nombre: "Software y Portales Web",
+    imagen: software,
+  },
+  {
+    nombre: "Planeación",
+    imagen: planeacion,
+  },
+  {
+    nombre: "Redes",
+    imagen: redes,
+  },
+  {
+    nombre: "Interventoria",
+    imagen: interventoria,
+  },
+];
 
-import danger from './image/danger.svg';
-import seguridad from './image/seguridad.png';
-import procesos from './image/procesos.png';
-import sistemas from './image/sistemas.png';
-import plan from './image/plan.png';
-import auditoria from './image/auditoria.png';
-import auditoria_sistemas from './image/auditoria_sistemas.png';
-import redes from './image/redes.png';
-import interventoria from './image/interventoria.png';
-import administracion from './image/administracion.png';
+const todosLosServicios = servicios.map(({ nombre, imagen }) => {
+  return (
+    <a href={`#${nombre}`} id="servicio-caja" key={nombre}>
+      <img className="servicio-icono" src={imagen} alt={nombre} />
+      <p>{nombre}</p>
+    </a>
+  );
+});
 
 export const Nosotros = () => {
   return (
-    <div className="nosotros-container-general" id="servicios">
-      {/* <img className="nosotros-container-general-img" src={logo} alt="logo"></img> */}
-      <div className="nosotros-container-compañia">
-        <h2 className="gcSeIN">Servicios</h2>
-        <div className="nosotros-container">
-          <a href="#Riesgos" className="nosotros-container-item">
-              <img 
-                src={danger}
-                alt="Gestion de Riesgos"
-                className="nosotros-container-item-icons"/>
-            <p>Gestion de Riesgos</p>
-          </a>
-          <a href="#AnalisSeguridad" className="nosotros-container-item">
-              <MdSecurity className="nosotros-container-item-icons"/>
-              <p>Analisis de Seguridad</p>
-          </a>
-          <a href="#Control" className="nosotros-container-item">
-              <VscSourceControl className="nosotros-container-item-icons"/>
-              <p>Control de Acesso</p>
-          </a>
-          <a href="#PSeguridad" className="nosotros-container-item">
-              <ImBooks className="nosotros-container-item-icons"/>
-              <p>Politicas de Seguridad</p>
-          </a>
-          <a href="#ISeguridad" className="nosotros-container-item">
-              <MdDangerous className="nosotros-container-item-icons"/>
-              <p>Incidentes de Seguridad</p>
-          </a>
-          <a href="#Proteccion" className="nosotros-container-item">
-              <AiOutlineFileProtect className="nosotros-container-item-icons" />
-              <p>Protección </p>
-          </a>
-          <a href="#PerSeguridad" className="nosotros-container-item">
-          <img 
-                src={seguridad}
-                alt="Gestion de Riesgos"
-                className="nosotros-container-item-icons"/>
-              <p>Personal de Seguridad </p>
-          </a>
-          <a href="#Auditoria" className="nosotros-container-item">
-          <img 
-                src={auditoria}
-                alt="Gestion de Riesgos"
-                className="nosotros-container-item-icons"/>
-              <p>Auditoria </p>
-          </a>
-          <a href="#Procesos" className="nosotros-container-item">
-          <img 
-                src={procesos}
-                alt="Gestion de Riesgos"
-                className="nosotros-container-item-icons"/>
-              <p>Procesos y Procedimientos </p>
-          </a>
-          <a href="#SGC" className="nosotros-container-item">
-          <img 
-                src={sistemas}
-                alt="Gestion de Riesgos"
-                className="nosotros-container-item-icons"/>
-              <p>SGC</p>
-          </a>
-          <a href="#AuditoriaSistemas" className="nosotros-container-item">
-          <img 
-                src={auditoria_sistemas}
-                alt="Gestion de Riesgos"
-                className="nosotros-container-item-icons"/>
-              <p>Auditoria de Sistemas</p>
-          </a>
-          <a href="#SoftwareWeb" className="nosotros-container-item">
-              <SiWebmoney className="nosotros-container-item-icons" />
-              <p>Sotfware y Portal Web</p>
-          </a>
-          <a href="#Planeacion" className="nosotros-container-item">
-          <img 
-                src={plan}
-                alt="Gestion de Riesgos"
-                className="nosotros-container-item-icons"/>
-              <p>Planeación</p>
-          </a>
-          <a href="#Redes" className="nosotros-container-item">
-          <img 
-                src={redes}
-                alt="Gestion de Riesgos"
-                className="nosotros-container-item-icons"/>
-              <p>Redes</p>
-          </a>
-          <a href="#Interventoria" className="nosotros-container-item">
-          <img 
-                src={interventoria}
-                alt="Gestion de Riesgos"
-                className="nosotros-container-item-icons"/>
-              <p>Interventoria</p>
-          </a>
-          <a href="#AdmiSistemas" className="nosotros-container-item">
-          <img 
-                src={administracion}
-                alt="Gestion de Riesgos"
-                className="nosotros-container-item-icons"/>
-              <p>Administración de Sistemas</p>
-          </a>
-        </div>
-      </div>
-    </div>
+    <section id="nosotros">
+      <h3>SERVICIOS</h3>
+      <div id="servicios-grid">{todosLosServicios}</div>
+    </section>
   );
 };
-
-// Nominacion de clases: BEM Block Element Modifier
--- src/components/main/servicios/Servicios.jsx
+++ src/components/main/servicios/Servicios.jsx
@@ -1,30 +1,26 @@
 import "./servicios.css";
-import IRiesgos from './image/GestionDeRiesgo.png';
-import IAnalisisSeguridad from './image/AnalisisDeSeguridad.png';
-import IControlDeAcesso from './image/ControlDeAcesso.png';
-import IPoliticas from './image/PoliticasDeSeguridad.png';
-import IMonitoreo from './image/MonitoreoDeSeguridad.png';
-import IProteccion from './image/Proteccion.png';
-import IPersonalSeguridad from './image/PersonalSeguridad.png';
-import IAuditoria from './image/Auditoria.png';
-import IProcesos from './image/Procesos.png';
-import IPortalWeb from './image/PortalesWeb.png';
-import IPlaneacion from './image/Planeacion.png';
-import IRedes from './image/redes.png';
-import IInterventoria from './image/interventoria.png';
-import ISistemas from './image/Sistemas.png';
-import Button from 'react-bootstrap/Button';
+import IRiesgos from "./image/GestionDeRiesgo.png";
+import IAnalisisSeguridad from "./image/AnalisisDeSeguridad.png";
+import IControlDeAcesso from "./image/ControlDeAcesso.png";
+import IPoliticas from "./image/PoliticasDeSeguridad.png";
+import IMonitoreo from "./image/MonitoreoDeSeguridad.png";
+import IProteccion from "./image/Proteccion.png";
+import IPersonalSeguridad from "./image/PersonalSeguridad.png";
+import IAuditoria from "./image/Auditoria.png";
+import IProcesos from "./image/Procesos.png";
+import IPortalWeb from "./image/PortalesWeb.png";
+import IPlaneacion from "./image/Planeacion.png";
+import IRedes from "./image/redes.png";
+import IInterventoria from "./image/interventoria.png";
+import ISistemas from "./image/Sistemas.png";
+import Button from "react-bootstrap/Button";
 
 export const Servicios = () => {
   return (
     <div>
       <div className="servicios-contenedor" id="Riesgos">
-        <img
-          className="servicios-contenedor-img"
-          src={IRiesgos}
-          alt="Logo"
-        />
-        <div className="servicios-contenedor-texto" >
+        <img className="servicios-contenedor-img" src={IRiesgos} alt="Logo" />
+        <div className="servicios-contenedor-texto">
           <h3>Gestion de Riesgos</h3>
           <ul className="servicios-contenedor-texto-caracteristicas">
             <li>Análisis de Riesgos.</li>
@@ -43,7 +39,7 @@ export const Servicios = () => {
           src={IAnalisisSeguridad}
           alt="Logo"
         />
-        <div className="servicios-contenedor-texto" >
+        <div className="servicios-contenedor-texto">
           <h3>Análisis de Seguridad</h3>
           <ul className="servicios-contenedor-texto-caracteristicas">
             <li>Gap del estado de seguridad</li>
@@ -72,11 +68,7 @@ export const Servicios = () => {
         </div>
       </div>
       <div className="servicios-contenedor">
-        <img
-          className="servicios-contenedor-img"
-          src={IPoliticas}
-          alt="Logo"
-        />
+        <img className="servicios-contenedor-img" src={IPoliticas} alt="Logo" />
         <div className="servicios-contenedor-texto" id="PSeguridad">
           <h3>Políticas de Seguridad</h3>
           <ul className="servicios-contenedor-texto-caracteristicas">
@@ -88,12 +80,8 @@ export const Servicios = () => {
         </div>
       </div>
       <div className="servicios-contenedor" id="ISeguridad">
-        <img
-          className="servicios-contenedor-img"
-          src={IMonitoreo}
-          alt="Logo"
-        />
-        <div className="servicios-contenedor-texto" >
+        <img className="servicios-contenedor-img" src={IMonitoreo} alt="Logo" />
+        <div className="servicios-contenedor-texto">
           <h3>Incidentes de Seguridad</h3>
           <ul className="servicios-contenedor-texto-caracteristicas">
             <li>Apoyo en incidentes de seguridad</li>
@@ -110,7 +98,7 @@ export const Servicios = () => {
           src={IProteccion}
           alt="Logo"
         />
-        <div className="servicios-contenedor-texto"  >
+        <div className="servicios-contenedor-texto">
           <h3>Protección</h3>
           <ul className="servicios-contenedor-texto-caracteristicas">
             <li>Diseño e implementación de controles de seguridad</li>
@@ -125,21 +113,20 @@ export const Servicios = () => {
           src={IPersonalSeguridad}
           alt="Logo"
         />
-        <div className="servicios-contenedor-texto"  >
+        <div className="servicios-contenedor-texto">
           <h3>Personal de Seguridad</h3>
           <ul className="servicios-contenedor-texto-caracteristicas">
-            <li>Recurso Especializado en Seguridad Informática de acuerdo a las directrices de las normas ISO 17799 - 27001 - 27002 (SGSI)</li>
+            <li>
+              Recurso Especializado en Seguridad Informática de acuerdo a las
+              directrices de las normas ISO 17799 - 27001 - 27002 (SGSI)
+            </li>
           </ul>
           <Button variant="success">Conoce más</Button>
         </div>
       </div>
       <div className="servicios-contenedor" id="Auditoria">
-        <img
-          className="servicios-contenedor-img"
-          src={IAuditoria}
-          alt="Logo"
-        />
-        <div className="servicios-contenedor-texto"  >
+        <img className="servicios-contenedor-img" src={IAuditoria} alt="Logo" />
+        <div className="servicios-contenedor-texto">
           <h3>Auditoria</h3>
           <ul className="servicios-contenedor-texto-caracteristicas">
             <li>Auditoria de cumplimiento de Seguridad</li>
@@ -150,61 +137,58 @@ export const Servicios = () => {
         </div>
       </div>
       <div className="servicios-contenedor" id="Procesos">
-        <img
-          className="servicios-contenedor-img"
-          src={IProcesos}
-          alt="Logo"
-        />
+        <img className="servicios-contenedor-img" src={IProcesos} alt="Logo" />
         <div className="servicios-contenedor-texto" id="ISeguridad">
           <h3>Procesos y Procedimientos</h3>
           <ul className="servicios-contenedor-texto-caracteristicas">
             <li>Diseño de procesos y procedimientos</li>
             <li>Diseño del mapa de Procesos</li>
             <li>Auditorias de calidad y control</li>
-            <li>Diseño e implementación del sistema de gestión de calidad (SGC)</li>
+            <li>
+              Diseño e implementación del sistema de gestión de calidad (SGC)
+            </li>
             <li>Diseño de politicas y objetivos de calidad</li>
           </ul>
           <Button variant="success">Conoce más</Button>
         </div>
       </div>
       <div className="servicios-contenedor" id="SGC">
-        <img
-          className="servicios-contenedor-img"
-          src={IMonitoreo}
-          alt="Logo"
-        />
+        <img className="servicios-contenedor-img" src={IMonitoreo} alt="Logo" />
         <div className="servicios-contenedor-texto">
           <h3>SGC.</h3>
           <ul className="servicios-contenedor-texto-caracteristicas">
-            <li>Diseño, implementación y puesta en marcha del sistema de gestión de calidad, de acuerdo a las directrices y políticas de la Norma ISO 9001 versióm 2000.</li>
+            <li>
+              Diseño, implementación y puesta en marcha del sistema de gestión
+              de calidad, de acuerdo a las directrices y políticas de la Norma
+              ISO 9001 versióm 2000.
+            </li>
           </ul>
           <Button variant="success">Conoce más</Button>
         </div>
       </div>
       <div className="servicios-contenedor" id="AuditoriaSistemas">
-        <img
-          className="servicios-contenedor-img"
-          src={IAuditoria}
-          alt="Logo"
-        />
+        <img className="servicios-contenedor-img" src={IAuditoria} alt="Logo" />
         <div className="servicios-contenedor-texto">
           <h3>Auditoria de Sistemas</h3>
           <ul className="servicios-contenedor-texto-caracteristicas">
-            <li>Recurso humano especializado en auditoria de sistemas y aplicacion de estándares internacionales a los sistemas de información de acuerdo a los modelos COBIT-COSO-MECI.</li>
+            <li>
+              Recurso humano especializado en auditoria de sistemas y aplicacion
+              de estándares internacionales a los sistemas de información de
+              acuerdo a los modelos COBIT-COSO-MECI.
+            </li>
           </ul>
           <Button variant="success">Conoce más</Button>
         </div>
       </div>
       <div className="servicios-contenedor" id="SoftwareWeb">
-        <img
-          className="servicios-contenedor-img"
-          src={IPortalWeb}
-          alt="Logo"
-        />
+        <img className="servicios-contenedor-img" src={IPortalWeb} alt="Logo" />
         <div className="servicios-contenedor-texto">
           <h3>Software y Portales Web</h3>
           <ul className="servicios-contenedor-texto-caracteristicas">
-            <li>Desarrollo y puesta en marcha de aplicaciones especializadas y especificas para su empresa</li>
+            <li>
+              Desarrollo y puesta en marcha de aplicaciones especializadas y
+              especificas para su empresa
+            </li>
             <li>Creación de portales corporativos y sistemas de información</li>
           </ul>
           <Button variant="success">Conoce más</Button>
@@ -219,26 +203,29 @@ export const Servicios = () => {
         <div className="servicios-contenedor-texto">
           <h3>Planeación</h3>
           <ul className="servicios-contenedor-texto-caracteristicas">
-            <li>Análisis, planeación y diseño de esquemas de contingencia, respaldo y recuperación a nivel de los datos dando continuidad al negocio</li>
+            <li>
+              Análisis, planeación y diseño de esquemas de contingencia,
+              respaldo y recuperación a nivel de los datos dando continuidad al
+              negocio
+            </li>
           </ul>
           <Button variant="success">Conoce más</Button>
         </div>
       </div>
       <div className="servicios-contenedor" id="Redes">
-        <img
-          className="servicios-contenedor-img"
-          src={IRedes}
-          alt="Logo"
-        />
+        <img className="servicios-contenedor-img" src={IRedes} alt="Logo" />
         <div className="servicios-contenedor-texto">
           <h3>Redes</h3>
           <ul className="servicios-contenedor-texto-caracteristicas">
-            <li>Diseño e implementación de redes alambricas e inalámbricas para la conexión y administración de recursos empresariales</li>
+            <li>
+              Diseño e implementación de redes alambricas e inalámbricas para la
+              conexión y administración de recursos empresariales
+            </li>
           </ul>
           <Button variant="success">Conoce más</Button>
         </div>
-        </div>
-        <div className="servicios-contenedor" id="Interventoria">
+      </div>
+      <div className="servicios-contenedor" id="Interventoria">
         <img
           className="servicios-contenedor-img"
           src={IInterventoria}
@@ -254,18 +241,20 @@ export const Servicios = () => {
           </ul>
           <Button variant="success">Conoce más</Button>
         </div>
-        </div>
-        <div className="servicios-contenedor" id="AdmiSistemas">
-        <img
-          className="servicios-contenedor-img"
-          src={ISistemas}
-          alt="Logo"
-        />
+      </div>
+      <div className="servicios-contenedor" id="AdmiSistemas">
+        <img className="servicios-contenedor-img" src={ISistemas} alt="Logo" />
         <div className="servicios-contenedor-texto">
           <h3>Administración de Sistemas</h3>
           <ul className="servicios-contenedor-texto-caracteristicas">
-            <li>Configuración de servidores: DNS, PROXY, FIREWALL, CORREO, TERMINAL SERVICE, VPN y de aplicación de base de datos</li>
-            <li>Admnistración, diseño y creación de planes de mantenimiento para vases de datos</li>
+            <li>
+              Configuración de servidores: DNS, PROXY, FIREWALL, CORREO,
+              TERMINAL SERVICE, VPN y de aplicación de base de datos
+            </li>
+            <li>
+              Admnistración, diseño y creación de planes de mantenimiento para
+              vases de datos
+            </li>
           </ul>
           <Button variant="success">Conoce más</Button>
         </div>
