9568c92562ec5677227b83d7171afad97b0356be
DavidTG7 <davidthekoa@gmail.com>
2023-01-09 21:52:33 -0500

    Arreglar bug en componente Ubicacion y bug en componente Empresa y darle formato correcto

--- src/components/main/empresa/Empresa.jsx
+++ src/components/main/empresa/Empresa.jsx
@@ -1,4 +1,3 @@
-// import Accordion from "react-bootstrap/Accordion";
 import {
   EmpresaMain,
   EmpresaSingleCard,
@@ -16,10 +15,6 @@ import objetivo from "./images/objetivo.jpg";
 
 export const Empresa = () => {
   return (
-    // <Accordion defaultActiveKey="0" style={{ textAlign: "justify" }}>
-    //   <Accordion.Item eventKey="0">
-    //     <Accordion.Header id="empresa">EMPRESA</Accordion.Header>
-    //     <Accordion.Body>
     <EmpresaMain>
       <EmpresaTitle>EMPRESA</EmpresaTitle>
       <hr />
@@ -64,27 +59,24 @@ export const Empresa = () => {
             lineamientos de las normas Internacionales ISO 9001, ISO 17799, ISO
             27001, ISO 27002, ISO 14000, que satisfagan las necesidades y
             expectativas de nuestros Clientes, para lo cual:
-            <br />
-            <br />
-            <ol type="a">
-              <li>
-                Contamos con un Recurso Humano competente, eficaz y comprometido
-                con el mejoramiento continuo de nuestros procesos y
-                procedimientos, cumpliendo con los requisitos establecidos en la
-                NTC-ISO 9001:2000, permitiéndonos ser competitivos, confiables,
-                seguros y consolidarnos en el sector Público y Privado, creando
-                una cultura de fomento empresarial.
-              </li>
-              <li>
-                El compromiso de la alta Gerencia es revisar, actualizar,
-                cumplir y hacer cumplir la presente Política de Calidad de
-                acuerdo con las necesidades de la comunidad, logrando con esto
-                aminorar las amenazas de riesgo relacionadas con la
-                Implementación y Mantenimiento del Sistema de Gestión de
-                Calidad.
-              </li>
-            </ol>
           </ItemParagraph>
+          <ol type="a">
+            <li>
+              Contamos con un Recurso Humano competente, eficaz y comprometido
+              con el mejoramiento continuo de nuestros procesos y
+              procedimientos, cumpliendo con los requisitos establecidos en la
+              NTC-ISO 9001:2000, permitiéndonos ser competitivos, confiables,
+              seguros y consolidarnos en el sector Público y Privado, creando
+              una cultura de fomento empresarial.
+            </li>
+            <li>
+              El compromiso de la alta Gerencia es revisar, actualizar, cumplir
+              y hacer cumplir la presente Política de Calidad de acuerdo con las
+              necesidades de la comunidad, logrando con esto aminorar las
+              amenazas de riesgo relacionadas con la Implementación y
+              Mantenimiento del Sistema de Gestión de Calidad.
+            </li>
+          </ol>
           <LowDivisor color="rgba(240, 31, 255, 1)" />
         </ItemTextBox>
       </EmpresaSingleCard>
@@ -100,53 +92,48 @@ export const Empresa = () => {
             NTC-ISO 9001:2000, y con el fin de incrementar el nivel de
             satisfacción de los Clientes y Usuarios de los servicios prestados,
             incluida la Comunidad, EAMT LTDA se propone:
-            <br />
-            <br />
-            <ol type="a">
-              <li>
-                Garantizar las características de calidad en cuanto a
-                oportunidad, seguridad, confiabilidad y asesoría establecidas
-                por la Empresa y ofrecidas a los clientes y público en general.
-              </li>
-              <li>
-                Establecer un adecuado y efectivo servicio de Satisfacción y
-                orientación al Cliente, por medio de la gestión y atención
-                oportuna de quejas, reclamos, sugerencias y consultas realizadas
-                por los Clientes tanto internos como externos.
-              </li>
-              <li>
-                Movilizar a la Empresa hacia la Excelencia y a la Calidad Total,
-                mediante la habilitación del trabajo, la aplicación tecnológica
-                e infraestructura adecuada y el desarrollo del Talento Humano.
-              </li>
-              <li>
-                Maximizar la eficiencia de la operación para disminuir costos,
-                para brindar asesorías y consultorías con altos índices de
-                calidad y control, así mismo aumentar las reservas de Capital de
-                la Empresa.
-              </li>
-              <li>
-                Mejorar y mantener la competencia y desempeño del Recurso Humano
-                que afecta directamente la Calidad del servicio, a través de
-                programas de Capacitación que afiancen sus Fortalezas y
-                Destrezas, y de un plan de Comunicación y Sensibilización en
-                donde comprendan la importancia de la Satisfacción del Cliente y
-                que entiendan el direccionamiento estratégico de la Empresa.
-              </li>
-              <li>
-                Mantener el nivel de mejora continua en los procesos del Sistema
-                de Gestión de la Calidad, identificando y previniendo los
-                riesgos inherentes a la operación, aplicando las acciones
-                necesarias para mitigar su impacto en la Gestión
-              </li>
-            </ol>
           </ItemParagraph>
+          <ol type="a">
+            <li>
+              Garantizar las características de calidad en cuanto a oportunidad,
+              seguridad, confiabilidad y asesoría establecidas por la Empresa y
+              ofrecidas a los clientes y público en general.
+            </li>
+            <li>
+              Establecer un adecuado y efectivo servicio de Satisfacción y
+              orientación al Cliente, por medio de la gestión y atención
+              oportuna de quejas, reclamos, sugerencias y consultas realizadas
+              por los Clientes tanto internos como externos.
+            </li>
+            <li>
+              Movilizar a la Empresa hacia la Excelencia y a la Calidad Total,
+              mediante la habilitación del trabajo, la aplicación tecnológica e
+              infraestructura adecuada y el desarrollo del Talento Humano.
+            </li>
+            <li>
+              Maximizar la eficiencia de la operación para disminuir costos,
+              para brindar asesorías y consultorías con altos índices de calidad
+              y control, así mismo aumentar las reservas de Capital de la
+              Empresa.
+            </li>
+            <li>
+              Mejorar y mantener la competencia y desempeño del Recurso Humano
+              que afecta directamente la Calidad del servicio, a través de
+              programas de Capacitación que afiancen sus Fortalezas y Destrezas,
+              y de un plan de Comunicación y Sensibilización en donde comprendan
+              la importancia de la Satisfacción del Cliente y que entiendan el
+              direccionamiento estratégico de la Empresa.
+            </li>
+            <li>
+              Mantener el nivel de mejora continua en los procesos del Sistema
+              de Gestión de la Calidad, identificando y previniendo los riesgos
+              inherentes a la operación, aplicando las acciones necesarias para
+              mitigar su impacto en la Gestión
+            </li>
+          </ol>
           <LowDivisor color="rgba(29, 250, 0, 1)" />
         </ItemTextBox>
       </EmpresaSingleCard>
     </EmpresaMain>
-    //     </Accordion.Body>
-    //   </Accordion.Item>
-    // </Accordion>
   );
 };
--- src/components/main/ubicacion/Ubicacion.jsx
+++ src/components/main/ubicacion/Ubicacion.jsx
@@ -6,11 +6,12 @@ export const Ubicacion = () => {
   return (
     <div className="Ubicacion">
       <iframe
+        title="mapa"
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.5405849340646!2d-75.59389478573432!3d6.192169128645449!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e46826eab24164b%3A0xb88e6bfad6aabc2e!2sCl.%2086%20%2351a-61%2C%20San%20Fernando%2C%20Itag%C3%BCi%2C%20Medell%C3%ADn%2C%20Antioquia!5e0!3m2!1ses!2sco!4v1671743014890!5m2!1ses!2sco"
         width="600"
         height="450"
         style={{ border: "0" }}
-        allowfullscreen=""
+        allowFullScreen=""
         loading="lazy"
         className="mapa"
       ></iframe>
