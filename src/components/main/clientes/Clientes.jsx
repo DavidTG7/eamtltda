381709b3397f49e036a83ba705f3de2296008ab7
DavidTG7 <davidthekoa@gmail.com>
2023-01-20 15:17:31 -0500

    Nueva propuesta para UI en el componente Clientes

--- src/components/main/clientes/Clientes.jsx
+++ src/components/main/clientes/Clientes.jsx
@@ -14,11 +14,12 @@ export const Clientes = () => {
         <hr />
         <div className="clientes__sectores">
           <p>
-            Hemos tenido la oportunidad de trabajar con los sectores: Público y Privado,
-            Corporativo Solidario, Financiero y Fondos de Empleados. Con una trayectoria superior a los 28 años.
+            {/* Hemos tenido la oportunidad de trabajar con los sectores: Público y Privado,
+            Corporativo Solidario, Financiero y Fondos de Empleados. Con una trayectoria superior a los 28 años. */}
+            Contamos con una trayectoria y experiencia superior a los 28 años trabajando con diversos clientes en los sectores:  
           </p>
         </div>
-        <div className="clientes__slider">
+        {/* <div className="clientes__slider">
           <div className="clientes__slider-fotos adelante1">
             {imagesPublico}
             {imagesPublico}
@@ -33,12 +34,12 @@ export const Clientes = () => {
             {imagenesEmpleados}
           </div>
           <div className="clientes__slider-cover" />
-        </div>
+        </div> */}
         {/* Secciones con titulo */}
-        <div className="clientes-contenedor-texto">
-          <h3>Sector Público y Privado</h3>
-          <LineDivisor color="rgba(189,25,230,0.8)"/>
-        </div>
+        {/* <div className="clientes-contenedor-texto"> */}
+          <h4 className="clientes__h4-1">Público y Privado</h4>
+          {/* <LineDivisor color="rgba(189,25,230,0.8)"/> */}
+        {/* </div> */}
         <div className="clientes__slider">
           <div className="clientes__slider-fotos adelante1">
             {imagesPublico}
@@ -46,10 +47,10 @@ export const Clientes = () => {
           </div>
           <div className="clientes__slider-cover" />
         </div>
-        <div className="clientes-contenedor-texto">
-          <h3>Sector Cooperativo Solidario y Financiero</h3>
-          <LineDivisor color="rgba(29, 250, 0, 1)"/>
-        </div>
+        {/* <div className="clientes-contenedor-texto"> */}
+          <h4 className="clientes__h4-2">Corporativo, Solidario y Financiero</h4>
+          {/* <LineDivisor color="rgba(29, 250, 0, 1)"/> */}
+        {/* </div> */}
         <div className="clientes__slider">
           <div className="clientes__slider-fotos atras1">
             {imagenesCorporativo}
@@ -57,10 +58,10 @@ export const Clientes = () => {
           </div>
           <div className="clientes__slider-cover" />
         </div>
-        <div className="clientes-contenedor-texto">
-          <h3>Fondo de Empleados</h3>
-          <LineDivisor color="rgba(20,234,209,0.8)"/>
-        </div>
+        {/* <div className="clientes-contenedor-texto"> */}
+          <h4 className="clientes__h4-3">Fondo de Empleados</h4>
+          {/* <LineDivisor color="rgba(20,234,209,0.8)"/> */}
+        {/* </div> */}
         <div className="clientes__slider">
           <div className="clientes__slider-fotos adelante2">
             {imagenesEmpleados}
