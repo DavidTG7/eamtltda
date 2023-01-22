f0746f45ba74178d631462b4d4a5879fc3603899
DavidTG7 <davidthekoa@gmail.com>
2023-01-22 16:37:25 -0500

    Nueva propuesta en componente Clientes

--- src/components/main/clientes/Clientes.jsx
+++ src/components/main/clientes/Clientes.jsx
@@ -5,8 +5,31 @@ import { imagenesCorporativo } from "./images/fotosCorporativo";
 import { LineDivisor } from "../servicios/servicios.styled";
 
 import "./clientes.css";
+import { useState } from "react";
 
 export const Clientes = () => {
+  const [isPublic, setIsPublic] = useState(true);
+  const [isCorporative, setIsCorporative] = useState(false);
+  const [isEmployees, setIsEmployees] = useState(false);
+
+  const handlePublic = () => {
+    setIsPublic(true);
+    setIsCorporative(false);
+    setIsEmployees(false);
+  };
+
+  const handleCorporative = () => {
+    setIsPublic(false);
+    setIsCorporative(true);
+    setIsEmployees(false);
+  };
+
+  const handleEmployees = () => {
+    setIsPublic(false);
+    setIsCorporative(false);
+    setIsEmployees(true);
+  };
+
   return (
     <>
       <div id="clientes" className="clientes__container">
@@ -16,18 +39,64 @@ export const Clientes = () => {
           <p>
             {/* Hemos tenido la oportunidad de trabajar con los sectores: Público y Privado,
             Corporativo Solidario, Financiero y Fondos de Empleados. Con una trayectoria superior a los 28 años. */}
-            Contamos con una trayectoria y experiencia superior a los 28 años trabajando con diversos clientes en los sectores:  
+            Contamos con una trayectoria y experiencia superior a los 28 años
+            trabajando con diversos clientes en los sectores:
           </p>
         </div>
-        {/* <div className="clientes__slider">
+        <div className="clientes__botones">
+          <button onClick={handlePublic}>PÚBLICO Y PRIVADO</button>
+          <button onClick={handleCorporative}>
+            CORPORATIVO SOLIDARIO Y FINANCIERO
+          </button>
+          <button onClick={handleEmployees}>FONDO DE EMPLEADOS</button>
+        </div>
+        {isPublic && (
+          <div className="clientes__slider">
+            <div className="clientes__slider-fotos adelante1">
+              {imagesPublico}
+              {imagesPublico}
+            </div>
+            <div className="clientes__slider-cover" />
+          </div>
+        )}
+        {isCorporative && (
+          <div className="clientes__slider">
+            <div className="clientes__slider-fotos atras1">
+              {imagenesCorporativo}
+              {imagenesCorporativo}
+            </div>
+            <div className="clientes__slider-cover" />
+          </div>
+        )}
+        {isEmployees && (
+          <div className="clientes__slider">
+            <div className="clientes__slider-fotos adelante2">
+              {imagenesEmpleados}
+              {imagenesEmpleados}
+              {imagenesEmpleados}
+            </div>
+            <div className="clientes__slider-cover" />
+          </div>
+        )}
+        
+        {/* <h4 className="clientes__h4-1">Público y Privado</h4>
+        <div className="clientes__slider">
           <div className="clientes__slider-fotos adelante1">
             {imagesPublico}
             {imagesPublico}
           </div>
+          <div className="clientes__slider-cover" />
+        </div>
+        <h4 className="clientes__h4-2">Corporativo, Solidario y Financiero</h4>
+        <div className="clientes__slider">
           <div className="clientes__slider-fotos atras1">
             {imagenesCorporativo}
             {imagenesCorporativo}
           </div>
+          <div className="clientes__slider-cover" />
+        </div>
+        <h4 className="clientes__h4-3">Fondo de Empleados</h4>
+        <div className="clientes__slider">
           <div className="clientes__slider-fotos adelante2">
             {imagenesEmpleados}
             {imagenesEmpleados}
@@ -35,41 +104,6 @@ export const Clientes = () => {
           </div>
           <div className="clientes__slider-cover" />
         </div> */}
-        {/* Secciones con titulo */}
-        {/* <div className="clientes-contenedor-texto"> */}
-          <h4 className="clientes__h4-1">Público y Privado</h4>
-          {/* <LineDivisor color="rgba(189,25,230,0.8)"/> */}
-        {/* </div> */}
-        <div className="clientes__slider">
-          <div className="clientes__slider-fotos adelante1">
-            {imagesPublico}
-            {imagesPublico}
-          </div>
-          <div className="clientes__slider-cover" />
-        </div>
-        {/* <div className="clientes-contenedor-texto"> */}
-          <h4 className="clientes__h4-2">Corporativo, Solidario y Financiero</h4>
-          {/* <LineDivisor color="rgba(29, 250, 0, 1)"/> */}
-        {/* </div> */}
-        <div className="clientes__slider">
-          <div className="clientes__slider-fotos atras1">
-            {imagenesCorporativo}
-            {imagenesCorporativo}
-          </div>
-          <div className="clientes__slider-cover" />
-        </div>
-        {/* <div className="clientes-contenedor-texto"> */}
-          <h4 className="clientes__h4-3">Fondo de Empleados</h4>
-          {/* <LineDivisor color="rgba(20,234,209,0.8)"/> */}
-        {/* </div> */}
-        <div className="clientes__slider">
-          <div className="clientes__slider-fotos adelante2">
-            {imagenesEmpleados}
-            {imagenesEmpleados}
-            {imagenesEmpleados}
-          </div>
-          <div className="clientes__slider-cover" />
-        </div>
       </div>
     </>
   );
