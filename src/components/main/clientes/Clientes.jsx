a2da26e47031c19c4548a402a995b9655253614d
DavidTG7 <davidthekoa@gmail.com>
2023-01-22 17:19:06 -0500

    Nueva propuesta para componente Clientes completa

--- src/components/main/clientes/Clientes.jsx
+++ src/components/main/clientes/Clientes.jsx
@@ -2,8 +2,6 @@ import { imagesPublico } from "./images/fotosIndex";
 import { imagenesEmpleados } from "./images/fotosEmpleados";
 import { imagenesCorporativo } from "./images/fotosCorporativo";
 
-import { LineDivisor } from "../servicios/servicios.styled";
-
 import "./clientes.css";
 import { useState } from "react";
 
@@ -37,18 +35,29 @@ export const Clientes = () => {
         <hr />
         <div className="clientes__sectores">
           <p>
-            {/* Hemos tenido la oportunidad de trabajar con los sectores: Público y Privado,
-            Corporativo Solidario, Financiero y Fondos de Empleados. Con una trayectoria superior a los 28 años. */}
             Contamos con una trayectoria y experiencia superior a los 28 años
             trabajando con diversos clientes en los sectores:
           </p>
         </div>
         <div className="clientes__botones">
-          <button onClick={handlePublic}>PÚBLICO Y PRIVADO</button>
-          <button onClick={handleCorporative}>
+          <button
+            className={isPublic && "clientes__botones-active"}
+            onClick={handlePublic}
+          >
+            PÚBLICO Y PRIVADO
+          </button>
+          <button
+            className={isCorporative && "clientes__botones-active"}
+            onClick={handleCorporative}
+          >
             CORPORATIVO SOLIDARIO Y FINANCIERO
           </button>
-          <button onClick={handleEmployees}>FONDO DE EMPLEADOS</button>
+          <button
+            className={isEmployees && "clientes__botones-active"}
+            onClick={handleEmployees}
+          >
+            FONDO DE EMPLEADOS
+          </button>
         </div>
         {isPublic && (
           <div className="clientes__slider">
@@ -78,32 +87,6 @@ export const Clientes = () => {
             <div className="clientes__slider-cover" />
           </div>
         )}
-        
-        {/* <h4 className="clientes__h4-1">Público y Privado</h4>
-        <div className="clientes__slider">
-          <div className="clientes__slider-fotos adelante1">
-            {imagesPublico}
-            {imagesPublico}
-          </div>
-          <div className="clientes__slider-cover" />
-        </div>
-        <h4 className="clientes__h4-2">Corporativo, Solidario y Financiero</h4>
-        <div className="clientes__slider">
-          <div className="clientes__slider-fotos atras1">
-            {imagenesCorporativo}
-            {imagenesCorporativo}
-          </div>
-          <div className="clientes__slider-cover" />
-        </div>
-        <h4 className="clientes__h4-3">Fondo de Empleados</h4>
-        <div className="clientes__slider">
-          <div className="clientes__slider-fotos adelante2">
-            {imagenesEmpleados}
-            {imagenesEmpleados}
-            {imagenesEmpleados}
-          </div>
-          <div className="clientes__slider-cover" />
-        </div> */}
       </div>
     </>
   );
